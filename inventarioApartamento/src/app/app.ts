e5b81f24d328a9cbfd781414e43b8f1509a7e801
Danielly <dani._sil@hotmail.com>
2025-10-04 23:08:29 -0300

    Add dashboard progress component with ng2-charts integration and update dependencies

--- inventarioApartamento/src/app/app.ts
+++ inventarioApartamento/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
+//Components
 import { Header } from './header/header';
 import { FooterMenu } from './footer-menu/footer-menu';
+import { DashboardProgress } from './dashboard-progress/dashboard-progress';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, FooterMenu],
+  imports: [RouterOutlet, Header, FooterMenu, DashboardProgress],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- inventarioApartamento/src/app/dashboard-progress/dashboard-progress.ts
+++ inventarioApartamento/src/app/dashboard-progress/dashboard-progress.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
 
+//Imports gráfico
+import { ChartData, Chart, DoughnutController, ArcElement, Tooltip, Legend, plugins } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(DoughnutController, ArcElement, Tooltip, Legend, ChartDataLabels);
+
 @Component({
   selector: 'app-dashboard-progress',
-  imports: [],
+  standalone: true,
+  imports: [BaseChartDirective],
   templateUrl: './dashboard-progress.html',
   styleUrl: './dashboard-progress.css'
 })
 export class DashboardProgress {
+  //Definindo tipo de gráfico
+  public doughnutChartType: any = 'doughnut';
+
+  //Dados do gráfico
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: ['Comprados', 'Não Comprados'],
+    datasets: [
+      {
+        data: [70, 30], //valor ******alterar para valor ser dinamico index 0 é maior, index 1 é menor
+        backgroundColor: ['#363636', '#F29F0F'], //cor gráfico
+        borderColor: 'transparent', //borda
+      }
+    ]
+  }
+
+  //Opções do gráfico
+  public doughnutChartOptions: any = {
+    responsive: true, //responsividade
+    cutout: '85%', //espessura do gráfico
+    plugins: {
+      legend: {
+        display: false, //desativando legenda
+      },
+      datalabels: {
+        display: false //desativando datalabels
+      },
+    },
+  }
+
+  //variaveis
+  public naoComprados: number = this.doughnutChartData.datasets[0].data[1]
+  public Comprados: number = this.doughnutChartData.datasets[0].data[0]
+
+  // Um método para formatar o número
+  formatarParaReal(valor: number): string {
+    return valor.toLocaleString('pt-BR');
+  }
+
+  public valorGasto: number = 45000; //****ajustar dinamicamente */
+  public valorRestante: number = 18000; //**ajustar dinamicamente */
 
 }
