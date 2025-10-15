9c9b9004c4495dc8b0a9001572df164437aae634
Danielly <dani._sil@hotmail.com>
2025-10-15 20:13:14 -0300

    Add ReportObra component and integrate into app; update ComodosList card styles

--- inventarioApartamento/src/app/app.ts
+++ inventarioApartamento/src/app/app.ts
@@ -7,10 +7,11 @@ import { Header } from './header/header';
 import { FooterMenu } from './footer-menu/footer-menu';
 import { DashboardProgress } from './dashboard-progress/dashboard-progress';
 import { ComodosList } from './comodos-list/comodos-list';
+import { ReportObra } from './report-obra/report-obra';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, DashboardProgress, ComodosList, FooterMenu],
+  imports: [RouterOutlet, Header, DashboardProgress, ComodosList, ReportObra, FooterMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- inventarioApartamento/src/app/report-obra/report-obra.ts
+++ inventarioApartamento/src/app/report-obra/report-obra.ts
@@ -8,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ReportObra {
 
+    // Um método para formatar o número
+  formatarParaReal(valor: number): string {
+    return valor.toLocaleString('pt-BR');
+  }
+
+  // ITENS OBRA
+    public valorEletricaIluminacao: number = 12000; //****ajustar dinamicamente */
+    public valorGesso: number = 5000; //****ajustar dinamicamente */
+    public valorPiso: number = 6000; //****ajustar dinamicamente */
+    public valorPintura: number = 2000; //****ajustar dinamicamente */
+    public valorMarcenaria: number = 90000; //****ajustar dinamicamente */
+    public valorVidros: number = 4000; //****ajustar dinamicamente */
+    public valorPedreiro: number = 10000; //****ajustar dinamicamente */
+
+    // VALOR FINAL
+    public valorFinal: number = this.valorEletricaIluminacao + this.valorGesso + this.valorPiso + this.valorPintura + this.valorMarcenaria + this.valorVidros + this.valorPedreiro
+ 
+
 }
