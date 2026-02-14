805e3f797ac18f1a905a29adad8bafc50a0116a2
Danielly <dani._sil@hotmail.com>
2026-02-14 14:03:05 -0300

    add button

--- inventarioApartamento/src/app/app.ts
+++ inventarioApartamento/src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./core/home-page/header/header";
+import { DashboardProgress } from "./core/home-page/dashboard-progress/dashboard-progress";
+import { ComodosList } from "./core/home-page/comodos-list/comodos-list";
+import { ReportObra } from "./core/home-page/report-obra/report-obra";
+import { FooterMenu } from "./shared/footer-menu/footer-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, DashboardProgress, ComodosList, ReportObra, FooterMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- inventarioApartamento/src/app/shared/footer-menu/footer-menu.ts
+++ inventarioApartamento/src/app/shared/footer-menu/footer-menu.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class FooterMenu {
 
+  public clickStart(): void {
+    console.log("Cliquei no inicio");
+  }
+
+  public clickWork(): void {
+    console.log("Cliquei no Diário de Obra");
+  }
+
+  public clickShopping(): void {
+    console.log("Cliquei no Enxoval");
+  }
+
+  public clickPrice(): void {
+    console.log("Cliquei no Comparativo de Preços");
+  }
 }
